7e4281bbc19380ddf0f8ab0c84630a99b4c5c093
Qamber Mehdi <qmehdi110@gmail.com>
2020-09-29 01:45:57 -0500

    /messages/sync endpoint working

--- whatsapp-backend/server.js
+++ whatsapp-backend/server.js
@@ -25,6 +25,16 @@ mongoose.connect(connection_url, {
 // API routes
 app.get("/", (req, res) => res.status(200).send("hello world"));
 
+app.get('/messages/sync', (req, res) => {
+    Messages.find((err, data) => {
+        if (err) {
+            res.status(500).send(err)
+        } else {
+            res.status(200).send(data)
+        }
+    })
+})
+
 app.post("/messages/new", (req, res) => {
   const dbMessage = req.body;
 
